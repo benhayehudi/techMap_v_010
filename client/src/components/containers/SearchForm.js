a56ae0320affdd00152f179025b885a993adc502
benhayehudi <bengreenberg@gmail.com>
2017-08-16 07:40:31 -0400

    working on search form

--- client/src/components/containers/SearchForm.js
+++ client/src/components/containers/SearchForm.js
@@ -2,32 +2,44 @@ import React from 'react';
 import {FormControl, FormGroup} from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const SearchForm = ({name, placeholder, value, onChange}) => {
+const onSubmit = (event) => {
+  event.preventDefault();
+}
+
+const onChange = (event) => {
+  this.setState({
+    input: event.target.value
+  })
+}
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ''
+    }
+  }
+
+  render() {
   return (
     <div className="col-lg-12">
       <div className="offset-lg-4">
-        <form>
+        <form onSubmit={this.onSubmit}>
           <FormGroup
           controlId="formBasicText">
             <h1>Search For A Job:</h1>
             <p id="form-helper-text"><em>(try something like "web developer jobs in Raleigh")</em></p>
               <FormControl
               type="text"
-              value="{this.state.value}"
               placeholder="Enter text"
-              onChange="{this.handleChange}" />
+              onChange={this.onChange} />
         </FormGroup>
       </form>
     </div>
   </div>
-  );
-};
-
-SearchForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func.isRequired
-};
+    );
+  };
+}
 
 export default SearchForm;
