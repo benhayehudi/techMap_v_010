8cee4b4b74e5f9f11cde95668afee6755545dd27
benhayehudi <bengreenberg@gmail.com>
2017-09-06 08:48:50 -0400

    added alt tags for images in contact component

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,9 +6,9 @@ class Contact extends Component {
     <div>
       <h1>Contact</h1>
       <br />
-      <a href="https://www.linkedin.com/in/rabbigreenberg/"><img src="linkedin-icon.png" /></a>
-      <a href="https://www.facebook.com/rabbigreenberg"><img src="facebook-icon.png" /></a>
-      <a href="https://twitter.com/RabbiGreenberg"><img src="twitter-icon.png" /></a>
+      <a href="https://www.linkedin.com/in/rabbigreenberg/"><img src="linkedin-icon.png" alt="linkedin icon" /></a>
+      <a href="https://www.facebook.com/rabbigreenberg"><img src="facebook-icon.png" alt="facebook icon" /></a>
+      <a href="https://twitter.com/RabbiGreenberg"><img src="twitter-icon.png" alt="twitter icon" /></a>
     </div>
     )
   }
