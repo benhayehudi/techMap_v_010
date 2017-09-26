8a2ca714031d7750c988c1ce194629c88a9ee65f
benhayehudi <bengreenberg@gmail.com>
2017-09-26 09:05:39 -0400

    added TextInput component, login with API actions

--- client/src/actions/LogInActions.js
+++
@@ -1,7 +0,0 @@
-export function logInUser() {
-  return (dispatch => {
-    fetch('http://localhost:3001/api/jobs/list')
-      .then(response => response.json())
-      .then(data => dispatch({ type: 'GET_SAVED_JOBS', data: data }))
-    }
-  )}
--- client/src/components/containers/LogInPage.js
+++ client/src/components/containers/LogInPage.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import TextInput from '../TextInput';
 import { connect } from 'react-redux';
-import { logInUser } from '../../actions/LogInActions';
+import {bindActionCreators} from 'redux';
+import * as sessionActions from '../../actions/SessionActions';
 
 class LogInPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      credentials: {
+        email: '',
+        password: ''
+      }
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSave = this.onSave.bind(this);
+  }
+
+  onChange(event) {
+    const field = event.target.name;
+    const credentials = this.state.credentials;
+    credentials[field] = event.target.value;
+    return this.setState({credentials: credentials});
+  }
+
+  onSave(event) {
+    event.preventDefault();
+    this.props.actions.logInUser(this.state.credentials);
+  }
 
   componentDidMount() {
 
@@ -13,16 +39,29 @@ class LogInPage extends React.Component {
     return (
       <div>
         <h1>Login:</h1>
-        
+        <TextInput
+          name="email"
+          label="email"
+          value={this.state.credentials.email}
+          onChange={this.onChange}/>
+
+        <TextInput
+          name="password"
+          label="password"
+          type="password"
+          value={this.state.credentials.password}
+          onChange={this.onChange}/>
+
+        <input type="submit"
+          className="btn btn-primary"
+          onClick={this.onSave}/>
     </div>
     );
   }
 }
-const mapStateToProps = (state) => {
-  return ({
-    savedJobs: state.savedJobs,
-    finishedLoading: state.finishedLoading
-  })
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(sessionActions, dispatch)
+  };
 }
-
-export default connect(mapStateToProps, { logInUser } )(LogInPage);
+export default connect(null, mapDispatchToProps)(LogInPage);
