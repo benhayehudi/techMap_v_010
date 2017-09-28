2b9aff018a894a1fef105241b9bb409a8d4f99df
benhayehudi <bengreenberg@gmail.com>
2017-09-28 11:40:32 -0400

    updated condition for auth protected view to render correctly if user not logged in

--- client/src/components/containers/JobsList.js
+++ client/src/components/containers/JobsList.js
@@ -6,37 +6,40 @@ import { getSavedJobs } from '../../actions/SearchActions';
 
 class JobsList extends React.Component {
 
-  componentDidMount() {
-    this.props.getSavedJobs();
-  }
+        componentDidMount() {
+            this.props.getSavedJobs();
+        }
 
-  render() {
-    var sortedJobs = this.props.savedJobs ?
-       this.props.savedJobs.sort(function(job1, job2) {
-        return job2.likes - job1.likes
-      })
-      :
-      null
-  const renderJobListCards =
-      this.props.savedJobs ?
-        sortedJobs.map(job =>
-          <JobListCard job={job} key={job.cacheId} />)
-      :
-        "Loading..."
-    return (
-      <div>
-        <SearchForm />
-        <h1>Saved Job Listings:</h1>
-        {renderJobListCards}
-    </div>
-    );
-  }
-}
-const mapStateToProps = (state) => {
-  return ({
-    savedJobs: state.savedJobs,
-    finishedLoading: state.finishedLoading
-  })
-}
+        render() {
+            var sortedJobs = this.props.savedJobs && this.props.logged_in ?
+                this.props.savedJobs.sort(function(job1, job2) {
+                    return job2.likes - job1.likes
+                }) :
+                null
+            const renderJobListCards =
+                this.props.savedJobs && this.props.logged_in ?
+                sortedJobs.map(job =>
+                    <
+                    JobListCard job = { job }
+                    key = { job.cacheId }
+                    />):
+                    "Loading... (If you are seeing this for more than a few seconds you are not logged in.)"
+                    return ( <
+                        div >
+                        <
+                        SearchForm / >
+                        <
+                        h1 > Saved Job Listings: < /h1> { renderJobListCards } < /
+                        div >
+                    );
+                }
+        }
+        const mapStateToProps = (state) => {
+            return ({
+                savedJobs: state.savedJobs,
+                finishedLoading: state.finishedLoading,
+                logged_in: state.session
+            })
+        }
 
-export default connect(mapStateToProps, { getSavedJobs } )(JobsList);
+        export default connect(mapStateToProps, { getSavedJobs })(JobsList);
