d15264eaff54c1a1a4cde0b6d57984f18e32ecec
benhayehudi <bengreenberg@gmail.com>
2017-09-28 12:14:37 -0400

    added user id column to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926121759) do
+ActiveRecord::Schema.define(version: 20170928161118) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170926121759) do
     t.string "cacheId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
